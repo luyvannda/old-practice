bd493b30d5bfaa53f9c7392954ef42c2fbca4f22
luyvannda <philips.joe329@gmail.com>
2023-06-15 16:44:59 +0700

    Create template for JavaScript practice session

--- js/node.js
+++ js/node.js
@@ -1,6 +1,4 @@
-let ourPlanet = "Earth";
+let a = +prompt("First number?", 1);
+let b = +prompt("Second number?", 2);
 
-let newUser = "Joe Joe";
-
-alert("Welcome " + newUser + " to planet " + ourPlanet + "!");
-
+alert(+a + +b); // 3
