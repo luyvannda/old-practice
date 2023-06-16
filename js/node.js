3fe7cb8e9f2580f12536f7507dc1e4dc19315acb
Luy Vannda <philips.joe329@gmail.com>
2023-06-16 12:03:06 +0700

    Do exercises related to string

--- js/node.js
+++ js/node.js
@@ -1,4 +1,5 @@
-let a = +prompt("First number?", 1);
-let b = +prompt("Second number?", 2);
+let text = "Apple, Banana, Kiwi";
+let textLength = text.length;
+console.log(textLength);
 
-alert(+a + +b); // 3
+let part = text.slice(-12, -6);
