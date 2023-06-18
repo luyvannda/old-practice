073884813b1077028bf55489020ae530948e6e62
luyvannda <philips.joe329@gmail.com>
2023-06-18 15:09:57 +0700

    practice change if...else to switch condition

--- js/node.js
+++ js/node.js
@@ -1,12 +1,21 @@
-// convert switch into if...else statement
-let browser = prompt("Which browser do you use", "Edge");
-if (browser === 'Edge') {
-  alert("You've got the Edge!");
-} else if (browser === 'Chrome'
-  || browser === 'Firefox'
-  || browser === 'Safari'
-  || browser === 'Opera') {
-  alert('Okay we support these browsers too');
-} else {
-  alert('We hope that this page looks ok!');
+// change this one into switch statement
+
+let a = +prompt('a?', '');
+
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+
+  case 1:
+    alert(1);
+    break;
+
+  case 2:
+  case 3:
+    alert('2,3');
+    break;
+
+  default:
+    alert("Not between 0 and 3");
 }
