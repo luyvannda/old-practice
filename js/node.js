4746eed859ac543c5da92c1773a7053527f9b0d8
luyvannda <philips.joe329@gmail.com>
2023-06-18 14:45:35 +0700

    learn to format code vertically to improve readibility

--- js/node.js
+++ js/node.js
@@ -1,18 +1,12 @@
-// convert this code into if...else statement
-
-
-switch (browser) {
-  case 'Edge':
-    alert("You've got the Edge!");
-    break;
-
-  case 'Chrome':
-  case 'Firefox':
-  case 'Safari':
-  case 'Opera':
-    alert('Okay we support these browsers too');
-    break;
-
-  default:
-    alert('We hope that this page looks ok!');
+// convert switch into if...else statement
+let browser = prompt("Which browser do you use", "Edge");
+if (browser === 'Edge') {
+  alert("You've got the Edge!");
+} else if (browser === 'Chrome'
+  || browser === 'Firefox'
+  || browser === 'Safari'
+  || browser === 'Opera') {
+  alert('Okay we support these browsers too');
+} else {
+  alert('We hope that this page looks ok!');
 }
